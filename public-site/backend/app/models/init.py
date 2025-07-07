b463e5228645fe91463076daff271487c9a96daf
Koosammy <87281182+Koosammy@users.noreply.github.com>
2025-07-07 04:40:42 +0000

    Update init.py

--- public-site/backend/app/models/init.py
+++ public-site/backend/app/models/init.py
@@ -1,4 +1,9 @@
-from .base import Base
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create Base here
+Base = declarative_base()
+
+# Import models after Base is created
 from .project import Project
 
 __all__ = ["Base", "Project"]
