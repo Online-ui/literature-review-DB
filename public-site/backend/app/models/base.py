5b1b7b3a3a65c78bebd9ef3b41aacaa7369b5283
Online-ui <j28210252@gmail.com>
2025-07-18 06:38:06 +0000

    Update base.py

--- public-site/backend/app/models/base.py
+++ public-site/backend/app/models/base.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Integer, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
 
-# Create the base class
 Base = declarative_base()
 
 class BaseModel(Base):
+    """Base model with common fields"""
     __abstract__ = True
     
     id = Column(Integer, primary_key=True, index=True)
